0550734b1cee8290513e331d8b11b3f0f93ac43b
guilhermeMars <guilhermemspiandorin@gmail.com>
2025-01-10 08:51:26 -0300

    feat: Update records integration

--- app.js
+++ app.js
@@ -67,8 +67,9 @@ async function create_invoice_odoo(uid, data) {
             x_studio_status: data.payment.status,
             x_studio_value: data.payment.value,
             x_studio_valor_lquido: data.payment.netValue,
-            ...(data.billingType && data.billingType !== "UNDEFINED"
-              ? { x_studio_forma_de_pagamento: data.billingType }
+            ...(data.payment.billingType &&
+            data.payment.billingType !== "UNDEFINED"
+              ? { x_studio_forma_de_pagamento: data.payment.billingType }
               : {}),
             x_studio_nmero_da_fatura: parseInt(data.payment.invoiceNumber) || 0,
           },
@@ -80,6 +81,81 @@ async function create_invoice_odoo(uid, data) {
         } else {
           resolve(createdId);
         }
+      }
+    );
+  });
+}
+
+async function update_invoice_odoo(uid, data) {
+  return new Promise((resolve, reject) => {
+    const object_client = create_client("/xmlrpc/2/object");
+    object_client.methodCall(
+      "execute_kw",
+      [
+        ODOO_DB,
+        uid,
+        ODOO_PASSWORD,
+        // Alterar
+        "x_receitas",
+        "search_read",
+        [[["x_name", "=", data.payment.id]]],
+        { limit: 1 },
+      ],
+      (err, recordIds) => {
+        if (err) {
+          return reject(err);
+        }
+        if (!recordIds || recordIds.length === 0) {
+          return reject(new Error("No matching records found for deletion"));
+        }
+
+        const recordId = recordIds[0];
+
+        // Em seguida, excluir os registros encontrados
+        object_client.methodCall(
+          "execute_kw",
+          [
+            ODOO_DB,
+            uid,
+            ODOO_PASSWORD,
+            "x_receitas",
+            "write",
+            [
+              [recordId.id],
+              {
+                x_studio_date: data.dateCreated,
+                x_name: data.payment.id,
+                x_studio_descrio: data.payment.description,
+                x_studio_id_1: data.payment.customer,
+                ...(data.payment.dueDate
+                  ? { x_studio_vencimento: data.payment.dueDate }
+                  : {}),
+                ...(data.payment.paymentDate
+                  ? { x_studio_data_de_pagamento: data.payment.paymentDate }
+                  : {}),
+                ...(data.payment.creditDate
+                  ? { x_studio_data_de_crdito: data.payment.creditDate }
+                  : {}),
+                x_studio_status: data.payment.status,
+                x_studio_value: data.payment.value,
+                x_studio_valor_lquido: data.payment.netValue,
+                ...(data.payment.billingType &&
+                data.payment.billingType !== "UNDEFINED"
+                  ? { x_studio_forma_de_pagamento: data.payment.billingType }
+                  : {}),
+                x_studio_nmero_da_fatura:
+                  parseInt(data.payment.invoiceNumber) || 0,
+              },
+            ],
+          ],
+          (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          }
+        );
       }
     );
   });
@@ -153,8 +229,22 @@ app.post("/create-invoice-odoo", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.post("/update-invoice-odoo", async (req, res) => {
+  try {
+    const { body } = req;
+    if (!body) {
+      return res
+        .status(400)
+        .json({ status: "error", message: `Missing request ${body}` });
+    }
+
+    const uid = await odoo_authenticate();
+    const updated_id = await update_invoice_odoo(uid, body);
+
+    res.json({ status: "success", updated_id });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
 });
 
 app.post("/delete-invoice-odoo", async (req, res) => {
@@ -173,4 +263,8 @@ app.post("/delete-invoice-odoo", async (req, res) => {
   } catch (error) {
     res.status(500).json({ status: "error", message: error.message });
   }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
