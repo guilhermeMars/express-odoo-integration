fba5b347df64a5da08dbb8cdbb686ce10285efc1
guilhermeMars <guilhermemspiandorin@gmail.com>
2025-01-15 13:53:17 -0300

    dev: Trying to discover asaas db userAgent

--- src/controllers/odooCobrancaController.js
+++ src/controllers/odooCobrancaController.js
@@ -8,6 +8,10 @@ import {
 export async function controller_create_cobranca(req, res) {
   try {
     const { body } = req;
+    const userAgent = req.headers["user-agent"];
+    const referer = req.headers["referer"]; // URL de onde veio a requisição
+    const origin = req.headers["origin"];
+
     if (!body) {
       return res
         .status(400)
@@ -17,7 +21,13 @@ export async function controller_create_cobranca(req, res) {
     const uid = await odoo_authenticate();
     const created_id = await create_cobranca_odoo(uid, body);
 
-    res.json({ status: "success", created_id });
+    const jsonData = {
+      userAgent: userAgent,
+      referer: referer,
+      origin: origin,
+    };
+
+    res.json({ status: "success", jsonData });
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: "error", message: error.message });
